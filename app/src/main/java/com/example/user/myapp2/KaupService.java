dfe7842c51d5e44e288de60663076d7358d01d95
andy3d <andylee3d@gmail.com>
2016-06-04 11:41:31 +0900

    카우프 프로그램

---
+++ app/src/main/java/com/example/user/myapp2/KaupService.java
@@ -0,0 +1,9 @@
+package com.example.user.myapp2;
+
+/**
+ * Created by USER on 2016-06-04.
+ */
+public class KaupService {
+    String name, result;
+    double weight,height;
+}
--- app/src/main/java/com/example/user/myapp2/MainActivity.java
+++ app/src/main/java/com/example/user/myapp2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.myapp2;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,45 +9,49 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends Activity {
+    EditText etName,etWeight,etHeight;
+    TextView resultCalc;
+    String name, result;
+    double weight,height;
+    int test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setTitle("==카우프프로그램==");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        etName = (EditText) findViewById(R.id.name);
+        etWeight= (EditText) findViewById(R.id.weight);
+        etHeight=(EditText) findViewById(R.id.height);
+        name=etName.getText().toString();
+        weight = Double.parseDouble(etWeight.getText().toString());
+        height = Double.parseDouble(etHeight.getText().toString());
+        resultCalc=(TextView)findViewById(R.id.resultCalc);
+        //test= integer.parselnt(etHeight.getText().toString());
+        int idx =(int) (weight/(height*height))*10000;
+        
+        if(idx>=30){
+            result="비만";
+        }else  if(idx>=24){
+            result="과체중";
+        }else  if(idx>=20){
+            result="정상";
+        }else  if(idx>=15){
+            result="저체중";
+        }else  if(idx>=13){
+            result="마름";
+        }else  if(idx>=10){
+            result="영양실조";
+        }else  if(idx>=24){
+            result="소모증";
         }
-
-        return super.onOptionsItemSelected(item);
+        resultCalc.setText(name+"님의 계산결과:"+result);
     }
+    
 }
